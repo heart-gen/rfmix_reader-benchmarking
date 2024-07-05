ef4e73c5d4585c4f79ee79c04ce98a1ed3d13091
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-07-04 20:15:36 -0400

    adding scripts for GPU version for two population simulation

---
+++ simulation/gpu_version/two_pop/_h/01.rfmix_reader.py
@@ -0,0 +1,31 @@
+# This script will test the memory usage and executive time for
+# RFMix-reader.
+import argparse
+from time import time
+from pyhere import here
+from rfmix_reader import read_rfmix
+from memory_profiler import profile
+
+@profile
+def read_data(prefix_path, BINARIES):
+    loci, rf_q, admix = read_rfmix(prefix_path, generate_binary=BINARIES)
+    admix = admix.compute()
+
+
+def main():
+    # Set up argument parsing
+    parser = argparse.ArgumentParser(
+        description='Measure memory usage and execution time.')
+    parser.add_argument('--binaries',  action="store_true",
+                        help='Create binaries')
+    # Parse the arguments
+    args = parser.parse_args()
+    prefix_path = here("input/simulations/two_populations/_m/")
+    start_time = time()
+    _ = read_data(prefix_path, args.binaries)
+    end_time = time()
+    print(f"Execution time: {end_time - start_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
