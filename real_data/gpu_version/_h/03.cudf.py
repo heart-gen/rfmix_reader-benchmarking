b3dd154782bc0ffab4731548b254a32f3e8f2830
KrotosBenjamin <kj.benjamin90@gmail.com>
2024-07-05 21:13:45 -0400

    adding scripts and log files for real data: GPU

---
+++ real_data/gpu_version/_h/03.cudf.py
@@ -0,0 +1,197 @@
+# This script will test the memory usage and executive time for
+# RFMix-reader.
+import pynvml
+from time import time
+from pyhere import here
+from cupy import ndarray
+from typing import Callable, List
+from torch.cuda import is_available
+from contextlib import contextmanager
+from rfmix_reader import get_prefixes
+from collections import OrderedDict as odict
+from cudf import DataFrame, read_csv, concat
+
+@contextmanager
+def gpu_memory_usage():
+    pynvml.nvmlInit()
+    handle = pynvml.nvmlDeviceGetHandleByIndex(0)  # Change the index if you have multiple GPUs
+    info_start = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    yield
+    info_end = pynvml.nvmlDeviceGetMemoryInfo(handle)
+    used_memory = info_end.used - info_start.used
+    print(f"GPU memory usage: {used_memory / 1024**2:.2f} MiB")
+    pynvml.nvmlShutdown()
+    
+
+def read_data(prefix_path, verbose=True):
+    fn = get_prefixes(prefix_path, verbose)
+    rf_q = _read_file(fn, lambda f: _read_Q(f["rfmix.Q"]))
+    pops = rf_q[0].drop(["sample_id", "chrom"], axis=1).columns.values
+    rf_q = concat(rf_q, axis=0, ignore_index=True)
+    X = _read_file(fn, lambda f: _read_fb(f["fb.tsv"]))
+    X = concat(X, axis=0, ignore_index=True)
+    loci = X.loc[:, ["chromosome", "physical_position"]]
+    X = X.iloc[:, 4:].values
+    admix = _subset_populations(X, len(pops))
+
+
+def _read_file(fn: List[str], read_func: Callable) -> List:
+    """
+    Read data from multiple files using a provided read function.
+
+    Parameters:
+    ----------
+    fn (List[str]): A list of file paths to read.
+    read_func (Callable): A function to read data from each file.
+
+    Returns:
+    -------
+    List: A list containing the data read from each file.
+    """
+    data = [];
+    for file_name in fn:
+        data.append(read_func(file_name))
+    return data
+
+
+def _read_fb(fn: str) -> DataFrame:
+    header = odict(_types(fn, False))
+    return _read_tsv(fn, header)
+
+
+def _read_Q(fn: str) -> DataFrame:
+    """
+    Read the Q matrix from a file and add the chromosome information.
+
+    Parameters:
+    ----------
+    fn (str): The file path of the Q matrix file.
+
+    Returns:
+    -------
+    DataFrame: The Q matrix with the chromosome information added.
+    """
+    from re import search    
+    header = odict(_types(fn))
+    df = _read_csv(fn, header)
+    match = search(r'chr(\d+)', fn)
+    if match:
+        chrom = match.group(0)
+        df["chrom"] = chrom
+    else:
+        print(f"Warning: Could not extract chromosome information from '{fn}'")
+    return df
+
+
+def _read_csv(fn: str, header: dict) -> DataFrame:
+    """
+    Read a CSV file into a pandas DataFrame with specified data types.
+
+    Parameters:
+    ----------
+    fn (str): The file path of the CSV file.
+    header (dict): A dictionary mapping column names to data types.
+
+    Returns:
+    -------
+    DataFrame: The data read from the CSV file as a pandas DataFrame.
+    """
+    if is_available():
+        df = read_csv(fn,sep="\t",header=None,names=list(header.keys()),
+                      dtype=header,comment="#")
+    else:
+        df = read_csv(fn,delim_whitespace=True,header=None,
+                      names=list(header.keys()),dtype=header,comment="#",
+                      compression=None, engine="c",iterator=False)
+    return df
+
+
+def _read_tsv(fn: str, header: dict) -> DataFrame:
+    """
+    Read a TSV file into a pandas DataFrame.
+
+    Parameters:
+    ----------
+    fn (str): File name of the TSV file.
+
+    Returns:
+    -------
+    DataFrame: DataFrame containing specified columns from the TSV file.
+    """
+    if is_available():
+        df = read_csv(fn,sep="\t",header=0, names=list(header.keys()),
+                      dtype=header,comment="#")
+    else:
+        chunks = read_csv(fn,delim_whitespace=True, header=0,
+                          names=list(header.keys()),dtype=header,comment="#",
+                          chunksize=100000)
+        # Concatenate chunks into single DataFrame
+        df = concat(chunks, ignore_index=True)
+    return df
+
+
+def _types(fn: str, Q: bool = True) -> dict:
+    """
+    Infer the data types of columns in a TSV file.
+
+    Parameters:
+    ----------
+    fn (str) : File name of the TSV file.
+
+    Returns:
+    -------
+    dict : Dictionary mapping column names to their inferred data types.
+    """
+    from pandas import StringDtype
+    if is_available():
+        df = read_csv(fn,sep="\t",nrows=2,skiprows=1)
+    else:
+        df = read_csv(fn,delim_whitespace=True,nrows=2,skiprows=1)
+    if Q:
+        # Initialize the header dictionary with the sample_id column
+        header = {"sample_id": StringDtype()}
+        # Update the header dictionary with the data types of the remaining columns
+        header.update(df.dtypes[1:].to_dict())
+    else:
+        header = df.dtypes.to_dict()
+    return header
+
+
+def _subset_populations(X: ndarray, npops: int) -> ndarray:
+    """
+    Subset and process the input array X based on populations.
+
+    Parameters:
+    X (dask.array): Input array where columns represent data for different populations.
+    npops (int): Number of populations for column processing.
+
+    Returns:
+    dask.array: Processed array with adjacent columns summed for each population subset.
+    """
+    from cupy import concatenate
+    pop_subset = []
+    pop_start = 0
+    ncols = X.shape[1]
+    if ncols % npops != 0:
+        raise ValueError("The number of columns in X must be divisible by npops.")
+    while pop_start < npops:
+        X0 = X[:, pop_start::npops] # Subset based on populations
+        if X0.shape[1] % 2 != 0:
+            raise ValueError("Number of columns must be even.")
+        X0_summed = X0[:, ::2] + X0[:, 1::2] # Sum adjacent columns
+        pop_subset.append(X0_summed)
+        pop_start += 1
+    return concatenate(pop_subset, 1)
+
+
+def main():
+    prefix_path = here("input/real_data/_m/")
+    start_time = time()
+    with gpu_memory_usage():
+        _ = read_data(prefix_path)
+    end_time = time()
+    print(f"Execution time: {end_time - start_time} seconds")
+
+
+if __name__ == "__main__":
+    main()
---
+++ real_data/gpu_version/_h/step_2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --partition=gpu,caracol
+#SBATCH --gpus=1
+#SBATCH --job-name=rfmix_gpu
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=jbenja13@jh.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=25gb
+#SBATCH --output=gpu_usage.no_binaries.log
+#SBATCH --time=01:00:00
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID}"
+
+## List current modules for reproducibility
+
+module list
+
+## Run job
+
+python ../_h/01.rfmix_reader.py
+
+echo "**** Job ends ****"
+date
