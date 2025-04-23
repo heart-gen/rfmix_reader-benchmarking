82f84ca3038bbbca9c754a178b2d5b259dde6bcf
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-04-22 19:13:55 -0500

    adding steps for processing simulated data; two population for Quest

---
+++ input/simulations/two_populations/_h/step_1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=normal
+#SBATCH --job-name=simu_genotypes
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=50gb
+#SBATCH --output=output.%A_%a.log
+#SBATCH --array=1:22
+#SBATCH --time=24:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module load htslib/1.16
+module list
+
+## Edit with your job command
+log_message "**** Loading mamba environment ****"
+source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda activate /projects/p32505/opt/env/AI_env
+
+log_message "**** Run simulation ****"
+ONE_K="/projects/b1213/resources/1kGP/"
+CHROM=${SLURM_ARRAY_TASK_ID}
+
+haptools simgenotype \
+         --model AFR_admixed.dat \
+         --mapdir ${ONE_K}/genetic_maps/_m/ \
+         --chroms ${CHROM} \
+         --seed 20240126 \
+         --ref_vcf ${ONE_K}/GRCh38_phased_vcf/raw/1kGP_high_coverage_Illumina.chr${CHROM}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz \
+         --sample_info 1k_sampleinfo.tsv \
+         --out ./chr${CHROM}.vcf.gz
+
+tabix -f chr${CHROM}.vcf.gz
+
+deactivate conda
+log_message "**** Job ends ****"
---
+++ input/simulations/two_populations/_h/step_2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=long
+#SBATCH --job-name=rfmix_simu
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100gb
+#SBATCH --output=rfmix.%A_%a.log
+#SBATCH --array=1:22
+#SBATCH --time=72:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module list
+
+## Edit with your job command
+CHROM=${SLURM_ARRAY_TASK_ID}
+SOFTWARE="/projects/p32505/opt/bin"
+ONE_K="/projects/b1213/resources/1kGP/"
+REF="/projects/b1213/resources/1kGP/GRCh38_phased_vcf/local-ancestry-ref"
+
+log_message "**** Run RFMix ****"
+echo -e "Chromosome: ${CRHOM}"
+
+$SOFTWARE/rfmix \
+    -f chr${CHROM}.vcf.gz \
+    -r $REF/1kGP_high_coverage_Illumina.chr${CHROM}.filtered.SNV_INDEL_SV_phased_panel.snpsOnly.eur.afr.vcf.gz \
+    -m $REF/samples_id2 \
+    -g $REF/genetic_map38 \
+    -o chr${CHROM} \
+    --chromosome=chr${CHROM}
+
+log_message "**** Job ends ****"
