1cea051ec8496ec227614567f251002480f45cb4
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-04-23 10:40:50 -0500

    rename full flare run

--- input/simulations/three_populations/_h/step_5.sh
+++ input/simulations/three_populations/_h/step_5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=b1042
-#SBATCH --partition=genomics
+#SBATCH --account=p32505
+#SBATCH --partition=normal
 #SBATCH --job-name=flare_prep
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kynon.benjamin@northwestern.edu
@@ -8,11 +8,9 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=16gb
-#SBATCH --output=logs/summary.%A-%a.log
+#SBATCH --output=logs/flare.%A-%a.log
 #SBATCH --array=2-22
 #SBATCH --time=24:00:00
-
-set -e
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
@@ -31,32 +29,29 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 ## List current modules for reproducibility
 
 module purge
+module load htslib/1.16
 module list
 
 ## Job commands here
+OUTDIR="flare-out"
 CHROM=${SLURM_ARRAY_TASK_ID}
 THREADS=${SLURM_CPUS_PER_TASK}
-OUTPUT_PREFIX="LIBD_TOPMed_AA_EA"
+OUTPUT_PREFIX="${flare-out}/simu_3pop"
 SOFTWARE="/projects/p32505/opt/bin"
-BASE_DIR="/projects/b1213/resources"
-MAP_DIR="${BASE_DIR}/1kGP/genetic_maps"
-REFERENCE_DIR="${BASE_DIR}/1kGP/GRCh38_phased_vcf"
-QUERY_DIR="${BASE_DIR}/libd_data/genotypes/combined_data/AA_EA/flare-vcf-no-missing"
+MAP_DIR="/projects/b1213/resources/1kGP/genetic_maps"
+
+log_message "**** Index reference VCF files ****"
+tabix -f ./temp/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz
 
 log_message "**** FLARE Local Ancestry Analysis ****"
-# Check if required files exist
-if [ ! -d "$REFERENCE_DIR" ]; then
-    log_message "Error: Reference directory not found: $REFERENCE_DIR"
-    exit 1
-fi
 
-if [ ! -d "$QUERY_DIR" ]; then
-    log_message "Error: Query file not found: $QUERY_DIR"
-    exit 1
-fi
-
-REF_VCF="1kGP_high_coverage_Illumina.chr${CHROM}.filtered.SNV_INDEL_SV_phased_panel.vcf.gz"
-QUERY_VCF="TOPMed_LIBD.AA_EA.chr${CHROM}.vcf.gz"
+java -Xmx16g -jar $SOFTWARE/flare.jar \
+     ref="./temp/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz" \
+     ref-panel="./temp/samples_id2" \
+     map="${MAP_DIR}/plink.chr${CHROM}.GRCh38.map" \
+     gt="chr${CHROM}.vcf.gz" nthreads=$THREADS \
+     seed=13131313 em=false model="${OUTPUT}"\
+     array=true out="${OUTPUT_PREFIX}"
 
 java -Xmx16g -jar $SOFTWARE/flare.jar \
      ref="${REFERENCE_DIR}/raw/${REF_VCF}" \
