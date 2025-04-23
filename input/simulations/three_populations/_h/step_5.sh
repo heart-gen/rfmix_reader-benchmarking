082e804fc4d85279f97479bd7224f53c98b33b23
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-04-23 11:02:02 -0500

    updating script for model location

--- input/simulations/three_populations/_h/step_5.sh
+++ input/simulations/three_populations/_h/step_5.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=p32505
-#SBATCH --partition=normal
+#SBATCH --partition=short
 #SBATCH --job-name=flare_prep
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kynon.benjamin@northwestern.edu
@@ -10,7 +10,7 @@
 #SBATCH --mem=16gb
 #SBATCH --output=logs/flare.%A-%a.log
 #SBATCH --array=2-22
-#SBATCH --time=24:00:00
+#SBATCH --time=03:00:00
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
@@ -36,29 +36,25 @@ module list
 OUTDIR="flare-out"
 CHROM=${SLURM_ARRAY_TASK_ID}
 THREADS=${SLURM_CPUS_PER_TASK}
-OUTPUT_PREFIX="${flare-out}/simu_3pop"
+OUTPUT_PREFIX="${OUTDIR}/simu_3pop"
 SOFTWARE="/projects/p32505/opt/bin"
 MAP_DIR="/projects/b1213/resources/1kGP/genetic_maps"
 
 log_message "**** Index reference VCF files ****"
 tabix -f ./temp/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz
 
+log_message "**** Fix PLINK map file ****"
+awk '{if(NR>0) $1="chr"$1; print}' "${MAP_DIR}/plink.chr${CHROM}.GRCh38.map" \
+    > ./temp/plink.chr${CHROM}.GRCh38.reformatted.map
+
 log_message "**** FLARE Local Ancestry Analysis ****"
 
 java -Xmx16g -jar $SOFTWARE/flare.jar \
      ref="./temp/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz" \
      ref-panel="./temp/samples_id2" \
-     map="${MAP_DIR}/plink.chr${CHROM}.GRCh38.map" \
+     map="./temp/plink.chr${CHROM}.GRCh38.reformatted.map" \
      gt="chr${CHROM}.vcf.gz" nthreads=$THREADS \
-     seed=13131313 em=false model="${OUTPUT}"\
-     array=true out="${OUTPUT_PREFIX}"
-
-java -Xmx16g -jar $SOFTWARE/flare.jar \
-     ref="${REFERENCE_DIR}/raw/${REF_VCF}" \
-     ref-panel="${REFERENCE_DIR}/local-ancestry-ref/samples_id2" \
-     map="${MAP_DIR}/plink.chr${CHROM}.GRCh38.map" \
-     gt="${QUERY_DIR}/_m/${QUERY_VCF}" nthreads=$THREADS \
-     seed=13131313 em=false model="/path/to/model" \
+     seed=13131313 em=false model="${OUTPUT}/simu_3pop.model" \
      array=true out="${OUTPUT_PREFIX}"
 
 log_message "**** Job ends ****"
