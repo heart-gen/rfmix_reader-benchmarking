fd2141733bec7822ac284ba3bc10eea3d2d73c1e
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-06-24 16:45:59 -0500

    revised to work; troubleshoot to use VCF input

--- input/simulations/two_populations/ground_truth/_h/01.convert_bp_to_snp.py
+++ input/simulations/two_populations/ground_truth/_h/01.convert_bp_to_snp.py
@@ -1,10 +1,10 @@
 import sys
+import pysam
 import pandas as pd
 
 def parse_bp_file(bp_file):
     ancestry_blocks = {}
     current_sample = None
-
     with open(bp_file, 'r') as f:
         for line in f:
             line = line.strip()
@@ -14,38 +14,43 @@ def parse_bp_file(bp_file):
             else:
                 ancestry, chrom, bp, cm = line.split()
                 ancestry_blocks[current_sample].append((int(bp), ancestry))
-                return ancestry_blocks
+    return ancestry_blocks
+
+
+def get_snp_positions_from_vcf(vcf_file):
+    vcf = pysam.VariantFile(vcf_file)
+    positions = [records.pos for records in vcf.fetch()]
+    return sorted(positions)
 
 
 def assign_ancestry_to_snps(snp_positions, ancestry_blocks):
     """
     Assigns ancestry to each SNP based on the block breakpoint structure.
     """
-    results = pd.DataFrame(index=snp_positions)
-    for sample, blocks in ancestry_blocks.items():
+    ancestry_matrix = {}
+    for sample_hap, blocks in ancestry_blocks.items():
         ancestry_per_snp = []
         block_idx = 0
         for snp in snp_positions:
-            # Move to the correct block
             while block_idx < len(blocks) and snp > blocks[block_idx][0]:
                 block_idx += 1
-            # Assign ancestry of the *previous* block
             if block_idx == 0:
                 ancestry = blocks[0][1]
             else:
                 ancestry = blocks[block_idx - 1][1]
             ancestry_per_snp.append(ancestry)
-        results[sample] = ancestry_per_snp
-    return results
+        ancestry_matrix[sample_hap] = ancestry_per_snp
+    results_df = pd.DataFrame.from_dict(ancestry_matrix, orient="columns")
+    results_df.index = snp_positions
+    results_df.index.name = "Position"
+    return results_df
 
 
 def main(bp_file, snp_file, output_file):
     # Read SNP positions file (1 column, no header)
-    snp_positions = pd.read_csv(snp_file, header=None)[0].tolist()
-
+    snp_positions = get_snp_positions_from_vcf(snp_file)
     ancestry_blocks = parse_bp_file(bp_file)
     per_snp_ancestry = assign_ancestry_to_snps(snp_positions, ancestry_blocks)
-    per_snp_ancestry.index.name = 'Position'
     per_snp_ancestry.to_csv(output_file, sep='\t')
 
 
