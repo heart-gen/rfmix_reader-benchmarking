664f24105fcfe816c2daeb64378f043370cb87ad
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-06-27 08:16:59 -0500

    updating submission script

---
+++ input/simulations/two_populations/ground_truth/_h/step_2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --account=p32505
+#SBATCH --partition=short
+#SBATCH --job-name=convert_int
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20gb
+#SBATCH --output=log_files/convert.%A_%a.log
+#SBATCH --array=1-22
+#SBATCH --time=01:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module list
+
+## Edit with your job command
+log_message "**** Loading mamba environment ****"
+source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda activate /projects/p32505/opt/env/AI_env
+
+log_message "**** Run conversion ****"
+CHROM=${SLURM_ARRAY_TASK_ID}
+
+python ../_h/02.convert_hap_to_geno.py ${CHROM} \
+       per_snp_ancestry.chr${CHROM}.tsv.gz chr${CHROM}.vcf.gz
+
+conda deactivate
+log_message "**** Job ends ****"
