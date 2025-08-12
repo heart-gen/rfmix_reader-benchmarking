0643a291ab6da83a0924b38b398bbaf9fcc540b9
mhi573 <mhurtado.jr0@gmail.com>
2025-08-12 15:03:47 -0500

    Adds indexed chr data and scripts

--- input/simulations/two_populations/ground_truth/_h/step_3.sh
+++ input/simulations/two_populations/ground_truth/_h/step_3.sh
@@ -1,59 +1,63 @@
 #!/bin/bash
 #SBATCH --account=p32505
-#SBATCH --partition=long
-#SBATCH --job-name=rfmix_ground
+#SBATCH --partition=short
+#SBATCH --job-name=index_chr
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=manuel.jr1@northwestern.edu
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=150gb
-#SBATCH --output=log_files/rfmix.%A_%a.log
+#SBATCH --cpus-per-task=1
 #SBATCH --array=1-22
-#SBATCH --time=72:00:00
+#SBATCH --mem=20gb
+#SBATCH --output=log_files/indexing.%A_%a.log
+#SBATCH --time=00:30:00
 
+# Function to echo with timestamp
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
 }
 
 log_message "**** Job starts ****"
 
+
 log_message "**** Quest info ****"
 echo "User: ${USER}"
 echo "Job id: ${SLURM_JOBID}"
 echo "Job name: ${SLURM_JOB_NAME}"
 echo "Node name: ${SLURM_NODENAME}"
-echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
 ## List current modules for reproducibility
 log_message "**** Loading modules ****"
 
 module purge
+module load htslib/1.16
 module list
 
-## Edit with your job command
 CHROM=${SLURM_ARRAY_TASK_ID}
-OUTDIR="rfmix-out"
-SOFTWARE="/projects/p32505/opt/bin"
-ONE_K="/projects/b1213/resources/1kGP/"
-REF="/projects/b1213/resources/1kGP/GRCh38_phased_vcf/local-ancestry-ref"
+VCF_FILE="chr${CHROM}.vcf.gz"
+TEMP_FILE="chr${CHROM}.vcf.gz.tmp"
 
-mkdir -p $OUTDIR
+log_message "Run job for ground-truth chromosome: ${CHROM}"
 
-log_message "**** Run RFMix ****"
-echo -e "Chromosome: ${CRHOM}"
+# Function to check BGZF
+is_bgzf() {
+    # Returns 0 if BGZF, 1 if not
+    if [[ $(htsfile "$1" 2>/dev/null) == *"BGZF"* ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# Set thread count for parallelization
-export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-THREADS=${SLURM_CPUS_PER_TASK}
+log_message "*** Checking compression format for $VCF_FILE ***"
+if ! is_bgzf "$VCF_FILE"; then
+    log_message "$VCF_FILE is not BGZF — re-compressing..."
+    gunzip -c "$VCF_FILE" | bgzip -c > "$TEMP_FILE" && mv "$TEMP_FILE" "$VCF_FILE"
+else
+    log_message "$VCF_FILE is already BGZF — skipping recompression."
+fi
 
-$SOFTWARE/rfmix \
-    -f chr${CHROM}.vcf.gz \
-    -r $REF/1kGP_high_coverage_Illumina.chr${CHROM}.filtered.SNV_INDEL_SV_phased_panel.snpsOnly.eur.afr.vcf.gz \
-    -m $REF/samples_id2 \
-    -g $REF/genetic_map38 \
-    -o ${OUTDIR}/chr${CHROM} \
-    --n-threads=${THREADS} \
-    --chromosome=chr${CHROM}
+log_message "*** Indexing VCF ***"
+tabix -p vcf "$VCF_FILE"
 
 log_message "**** Job ends ****"
