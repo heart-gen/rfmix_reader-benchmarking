f14a56c6addc1175032a194f967fabf702171806
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-08-12 17:27:56 -0500

    rerun failed

---
+++ input/simulations/three_populations/ground_truth/_h/step_2b.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --account=b1042
+#SBATCH --partition=genomics
+#SBATCH --job-name=convert_int
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kynon.benjamin@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=30gb
+#SBATCH --output=log_files/convert.%A_%a.log
+#SBATCH --array=4,14
+#SBATCH --time=4:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module load gcc/12.3.0-gcc
+module list
+
+## Edit with your job command
+log_message "**** Loading mamba environment ****"
+source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda activate /projects/p32505/opt/env/AI_env
+
+log_message "**** Run conversion ****"
+CHROM=${SLURM_ARRAY_TASK_ID}
+
+python ../_h/02.convert_hap_to_geno.py ${CHROM} \
+       per_snp_ancestry.chr${CHROM}.tsv.gz chr${CHROM}.vcf.gz 10000
+
+conda deactivate
+log_message "**** Job ends ****"
