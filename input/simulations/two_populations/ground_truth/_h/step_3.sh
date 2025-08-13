a866d0403acbfe0f5b2fd40e6523f18261914a02
mhi573 <mhurtado.jr0@gmail.com>
2025-08-13 11:11:48 -0500

    Adds submission script for computing global ancestry

---
+++ input/simulations/two_populations/ground_truth/_h/step_3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --account=b1042
+#SBATCH --partition=genomics
+#SBATCH --job-name=compute_glob_anc
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=manuel.jr1@northwestern.edu
+#SBATCH --nodes=1
+#SBATCH --mem=20gb
+#SBATCH --output=log_files/comp_glob_anc.%J.log
+#SBATCH --time=1:00:00
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Quest info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+## List current modules for reproducibility
+log_message "**** Loading modules ****"
+
+module purge
+module load gcc/12.3.0-gcc
+module list
+
+## Edit with your job command
+log_message "**** Loading mamba environment ****"
+source /projects/p32505/opt/miniforge3/etc/profile.d/conda.sh
+conda activate /projects/p32505/opt/env/AI_env
+
+log_message "**** Run script ****"
+
+python ../_h/
+
+conda deactivate
+log_message "**** Job ends ****"
