65b079f50b1c9c5ba320f7cf624d902cf0f3f286
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-08-26 20:07:44 -0500

    revised script to improve FLARE run; resolves #14

--- input/simulations/three_populations/_h/step_4.sh
+++ input/simulations/three_populations/_h/step_4.sh
@@ -29,6 +29,7 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 ## List current modules for reproducibility
 
 module purge
+module load bcftools/1.10.1
 module load htslib/1.16
 module list
 
@@ -41,18 +42,35 @@ THREADS=${SLURM_CPUS_PER_TASK}
 SOFTWARE="/projects/p32505/opt/bin"
 MAPDIR="/projects/b1213/resources/1kGP/genetic_maps"
 
-mkdir -p $OUTDIR
+mkdir -p "$OUTDIR"
+mkdir -p "$TEMPDIR"
 
-log_message "**** Index reference VCF files ****"
-tabix -f -p vcf ${TEMPDIR}/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz
+log_message "**** Match Variants ****"
+bcftools query -f '%CHROM\t%POS\t%REF\t%ALT\n' \
+         "${TEMPDIR}/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz" > \
+         "${TEMPDIR}/ref.chr${CHROM}.variants.txt"
+
+bcftools view -R "${TEMPDIR}/ref.chr${CHROM}.variants.txt" \
+         "${INPUTS}/chr${CHROM}.vcf.gz" | \
+    bcftools sort | \
+    bcftools norm -d both -Oz -o "${INPUTS}/chr${CHROM}.filtered.vcf.gz"
+
+bcftools view -R "${TEMPDIR}/ref.chr${CHROM}.variants.txt" \
+         "${TEMPDIR}/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz" | \
+    bcftools sort | \
+    bcftools norm -d both -Oz -o "${TEMPDIR}/ref.chr${CHROM}.filtered.vcf.gz"
+
+log_message "**** Index VCF files ****"
+tabix -f -p vcf "${TEMPDIR}/ref.chr${CHROM}.filtered.vcf.gz"
+tabix -f -p vcf "${INPUTS}/chr${CHROM}.filtered.vcf.gz"
 
 log_message "**** FLARE Local Ancestry Analysis ****"
 
 java -Xmx24g -jar $SOFTWARE/flare.jar \
-     ref="${TEMPDIR}/1kGP.chr${CHROM}.filtered.snpsOnly.afr_washington.vcf.gz" \
+     ref="${TEMPDIR}/ref.chr${CHROM}.filtered.vcf.gz" \
      ref-panel="${TEMPDIR}/samples_id2" nthreads=$THREADS \
      map="${MAPDIR}/plink.chr${CHROM}.GRCh38.reformatted.map" \
-     gt="${INPUTS}/chr${CHROM}.vcf.gz" em=false gen=10 \
+     gt="${INPUTS}/chr${CHROM}.filtered.vcf.gz" em=false gen=10 \
      seed=13131313 array=true out="${OUTDIR}/chr${CHROM}"
 
 log_message "**** Job ends ****"
