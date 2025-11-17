e3305071c4c2e000061e50bc503bf002be84063c
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-11-17 18:46:45 -0500

    update to change directory based on task ID

--- benchmarks/_rfmix_cpu/_h/step_1b.sh
+++ benchmarks/_rfmix_cpu/_h/step_1b.sh
@@ -36,8 +36,24 @@ conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
 
 log_message "**** Run analysis ****"
 TASK="${SLURM_ARRAY_TASK_ID}"
-INPUT_DIR="../../../input/simulations/two_populations/_m/rfmix-out/"
 OUTDIR="two_pop/no_binaries"
+
+# Choose input directory based on TASK
+case "${TASK}" in
+  1)
+    INPUT_DIR="../../../input/simulations/two_populations/task_1/rfmix-files/"
+    ;;
+  2)
+    INPUT_DIR="../../../input/simulations/two_populations/task_2/rfmix-files/"
+    ;;
+  3)
+    INPUT_DIR="../../../input/simulations/two_populations/_m/rfmix-out/"
+    ;;
+  *)
+    echo "Invalid TASK value: ${TASK}"
+    exit 1
+    ;;
+esac
 
 python ../_h/01.rfmix_parsing.py --input "${INPUT_DIR}" \
        --output "${OUTDIR}" --label "task_${TASK}" --task "${TASK}"
