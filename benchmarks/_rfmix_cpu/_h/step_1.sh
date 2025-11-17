fd74ae8f70c025433d5b5f1d9e3311ecf330d547
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-11-17 17:47:02 -0500

    replace submission script with CPU version

--- benchmarks/_rfmix_cpu/_h/step_1.sh
+++ benchmarks/_rfmix_cpu/_h/step_1.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH --partition=GPU-shared
-#SBATCH --job-name=cudf_3pop
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=pandas_2pop
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kj.benjamin90@gmail.com
-#SBATCH --gpus=v100-32:1
-#SBATCH --ntasks-per-node=5
+#SBATCH --ntasks-per-node=64
 #SBATCH --array=1-3
-#SBATCH --time=06:00:00
-#SBATCH --output=logs/cudf.three_pop.%A_%a.log
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/pandas.two_pop.%A_%a.log
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
@@ -30,7 +29,6 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
 module purge
 module load anaconda3/2024.10-1
-module load cuda
 module list
 
 log_message "**** Loading conda environment ****"
@@ -38,10 +36,10 @@ conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
 
 log_message "**** Run analysis ****"
 TASK="${SLURM_ARRAY_TASK_ID}"
-INPUT_DIR="../../../input/simulations/three_populations/_m/rfmix-files/"
+INPUT_DIR="../../../input/simulations/two_populations/_m/rfmix-out/"
 
-python ../_h/01.cudf_parsing.py --input "${INPUT_DIR}" \
-       --output "three_pop" --label "task_${TASK}" --task "${TASK}"
+python ../_h/01.pandas_parsing.py --input "${INPUT_DIR}" \
+       --output "two_pop" --label "task_${TASK}" --task "${TASK}"
 
 if [ $? -ne 0 ]; then
     echo "Python script failed. Check the error logs."
