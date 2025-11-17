249e816b60bb627ff63c3384b6c7e98dcd91553a
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-11-17 14:05:40 -0500

    increasing compute time

--- benchmarks/_polars/_h/step_1.sh
+++ benchmarks/_polars/_h/step_1.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-user=kj.benjamin90@gmail.com
 #SBATCH --ntasks-per-node=64
 #SBATCH --array=1-3
-#SBATCH --time=02:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=logs/polars.two_pop.%A_%a.log
 
 log_message() {
