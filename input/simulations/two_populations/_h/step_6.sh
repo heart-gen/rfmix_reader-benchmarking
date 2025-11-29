4c55a284cb73be2141d46d73a9a4d646160a764c
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-11-29 08:13:45 -0500

    updating scripts to generate plink files for loci imputation

--- input/simulations/two_populations/_h/step_6.sh
+++ input/simulations/two_populations/_h/step_6.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH --account=p32505
-#SBATCH --partition=short
-#SBATCH --job-name=clean_files
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=gen_plinks
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=kynon.benjamin@northwestern.edu
-#SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=2gb
-#SBATCH --output=clean.log
+#SBATCH --mail-user=kj.benjamin90@gmail.com
+#SBATCH --ntasks-per-node=32
+#SBATCH --array=1-22
 #SBATCH --time=01:00:00
+#SBATCH --output=logs/plink.%A_%a.log
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
@@ -27,13 +25,24 @@ echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 ## List current modules for reproducibility
 
 module purge
+module load anaconda3/2024.10-1
 module list
 
 ## Edit with your job command
-log_message "**** Clean-up files ****"
 
-rm out*log
-rm *bp *gz *tbi
-rm 1k_sampleinfo.tsv AFR_admixed.dat
+# path to your contigs file
+VCFDIR="gt-files"
+OUTDIR="plink-files"
+CHR=${SLURM_ARRAY_TASK_ID}
 
+mkdir -p ${OUTDIR}
+
+log_message "Processing ${CHR} ..."
+
+log_message "**** Loading conda environment ****"
+conda activate /ocean/projects/bio250020p/shared/opt/env/genomics
+
+plink2 --vcf "${VCFDIR}/chr${CHR}.vcf.gz" --make-pgen --out "${OUTDIR}/chr${CHR}"
+
+conda deactivate
 log_message "**** Job ends ****"
