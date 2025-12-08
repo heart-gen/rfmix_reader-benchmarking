6d496894c1c01dd00f0b8e44a70263378e5dc136
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-12-08 15:38:20 -0500

    updated for CLI

--- loci_imputation_comp/locus_level/_h/step_0.sh
+++ loci_imputation_comp/locus_level/_h/step_0.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=RM-shared
-#SBATCH --job-name=locus_3pop
+#SBATCH --job-name=create_binaries
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=kj.benjamin90@gmail.com
 #SBATCH --ntasks-per-node=64
-#SBATCH --time=12:00:00
-#SBATCH --output=logs/locus_level.three_pop.%J.log
+#SBATCH --time=03:00:00
+#SBATCH --output=logs/create_binaries.three_pop.%J.log
 
 log_message() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
@@ -17,7 +17,6 @@ log_message "**** Bridges-2 info ****"
 echo "User: ${USER}"
 echo "Job id: ${SLURM_JOBID}"
 echo "Job name: ${SLURM_JOB_NAME}"
-echo "Node name: ${SLURM_NODENAME}"
 echo "Hostname: ${HOSTNAME}"
 echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
 
@@ -29,13 +28,9 @@ log_message "**** Loading conda environment ****"
 conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
 
 log_message "**** Run analysis ****"
-TASK="${SLURM_ARRAY_TASK_ID}"
-OUTDIR="three_pop"
 RFMIX_DIR="input/simulations/three_populations/_m/rfmix-files/"
-SIMU_DIR="input/simulations/three_populations/_m/gt-files/"
 
-python ../_h/01.rfmix_parsing.py --input "${INPUT_DIR}" \
-       --output "${OUTDIR}" --label "task_${TASK}" --task "${TASK}" --binaries
+create-binaries --binary_dir "${RFMIX_DIR}/binary_files" "${RFMIX_DIR}"
 
 if [ $? -ne 0 ]; then
     echo "Python script failed. Check the error logs."
