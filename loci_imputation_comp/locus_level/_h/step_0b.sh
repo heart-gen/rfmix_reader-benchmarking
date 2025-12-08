79905d1cb1dd151128bdfb1e87d7d7473e80f3db
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-12-08 15:39:24 -0500

    creating step A and B for creating binaries

---
+++ loci_imputation_comp/locus_level/_h/step_0b.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=create_binaries
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kj.benjamin90@gmail.com
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=03:00:00
+#SBATCH --output=logs/create_binaries.three_pop.%J.log
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Bridges-2 info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+module purge
+module load anaconda3/2024.10-1
+module list
+
+log_message "**** Loading conda environment ****"
+conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
+
+log_message "**** Run analysis ****"
+RFMIX_DIR="input/simulations/three_populations/_m/rfmix-files/"
+
+create-binaries --binary_dir "${RFMIX_DIR}/binary_files" "${RFMIX_DIR}"
+
+if [ $? -ne 0 ]; then
+    echo "Python script failed. Check the error logs."
+    exit 1
+fi
+
+conda deactivate
+log_message "Job finished at: $(date)"
