f54898f5edfa1530079296cd2c24e90ef51a82c2
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-12-11 08:17:07 -0500

    adding phasing scripts for real data

---
+++ input/aanri_data/rfmix-version/_h/03.phase_data.py
@@ -0,0 +1,49 @@
+import logging
+import argparse
+import session_info
+from pyhere import here
+from pathlib import Path
+from rfmix_reader import phase_rfmix_chromosome_to_zarr
+
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+    )
+
+
+def parse_parameters():
+    parser = argparse.ArgumentParser(description="Locus-Level Imputation Accuracy")
+    parser.add_argument("--rfmix-input", type=Path, required=True)
+    parser.add_argument("--ref-input", type=Path, default=Path("input/references/_m"))
+    parser.add_argument("--chrom", type=int, default=21)
+    return parser.parse_args()
+
+
+def main():
+    configure_logging()
+    args = parse_parameters()
+
+    # Phase output per chromosome
+    logging.info("Phase RFMix outputs per chromosome...")
+    binary_path = args.rfmix_input / "binary_files"
+    phased_path = args.rfmix_input / "phased_files"
+    ref_zarr = args.ref_input / "reference_zarr" / f"chr{args.chrom}.zarr"
+    sample_annot_path = args.ref_input / "samples_id2"
+    output_path = f"{phased_path}/phased_chr{args.chrom}.zarr"
+    _ = phase_rfmix_chromosome_to_zarr(
+        file_prefix=here(args.rfmix_input),
+        ref_zarr_root=here(ref_zarr),
+        binary_dir=here(binary_path),
+        sample_annot_path=here(sample_annot_path),
+        output_path=here(output_path),
+        chrom=str(args.chrom),
+    )
+
+    # Session information
+    session_info.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ input/aanri_data/rfmix-version/_h/step_3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --partition=RM-shared
+#SBATCH --job-name=phase_data
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kj.benjamin90@gmail.com
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=4:00:00
+#SBATCH --array=1-22
+#SBATCH --output=logs/phase.annri_data.%A_%a.log
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+log_message "**** Job starts ****"
+
+log_message "**** Bridges-2 info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOBID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURM_NODENAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SLURM_ARRAY_TASK_ID:-N/A}"
+
+module purge
+module load anaconda3/2024.10-1
+module list
+
+log_message "**** Loading conda environment ****"
+conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
+
+log_message "**** Run analysis ****"
+RFMIX_DIR="input/aanri_data/rfmix-version/_m"
+
+python ../_h/03.phase_data.py \
+       --rfmix-input "${RFMIX_DIR}" \
+       --chrom ${SLURM_ARRAY_TASK_ID}
+
+if [ $? -ne 0 ]; then
+    echo "Python script failed. Check the error logs."
+    exit 1
+fi
+
+conda deactivate
+log_message "Job finished at: $(date)"
