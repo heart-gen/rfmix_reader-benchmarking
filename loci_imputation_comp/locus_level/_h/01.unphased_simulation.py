fb70f108f36e63d3da321faad7297df48169b160
Kynon J Benjamin <kj.benjamin90@gmail.com>
2025-12-12 07:55:52 -0500

    Update unphased simulation to load imputed data

--- loci_imputation_comp/locus_level/_h/01.unphased_simulation.py
+++ loci_imputation_comp/locus_level/_h/01.unphased_simulation.py
@@ -16,8 +16,7 @@ from sklearn.metrics import (
     matthews_corrcoef
 )
 
-from localqtl import PgenReader
-from rfmix_reader import read_rfmix, read_simu, interpolate_array
+from rfmix_reader import read_simu
 
 def configure_logging():
     logging.basicConfig(
@@ -33,28 +32,12 @@ def parse_parameters():
     parser.add_argument("--population", type=str, choices=["two","three"], default="three")
     parser.add_argument("--method", type=str, choices=["linear", "nearest", "stepwise"],
                         default="linear")
+    parser.add_argument("--output", type=Path, required=True)
     return parser.parse_args()
-
-
-def load_variants(plink_path):
-    pgr = PgenReader(plink_path)
-    variant_df = pgr.variant_df.copy()
-    variant_df.loc[:, "chrom"] = "chr" + variant_df.chrom
-    return variant_df[~variant_df.duplicated(subset=["chrom", "pos"], keep="first")]
 
 
 def standardize_variant_columns(df):
     return df.rename(columns={"chromosome": "chrom", "physical_position": "pos"})
-
-
-def impute_data(loci_df, variants, admix, zarr_path, method):
-    if hasattr(loci_df, "to_pandas"):
-        loci_df = loci_df.to_pandas()
-    variant_df = variants.merge(loci_df, on=["chrom", "pos"], how="outer")
-    variant_df = variant_df.loc[:, ["chrom", "pos", "i"]]
-    _ = interpolate_array(variant_df, admix, zarr_outdir=zarr_path,
-                          interpolation=method, use_bp_positions=True)
-    return variant_df
 
 
 def align_variants(df_ref, df_target):
@@ -166,27 +149,16 @@ def main():
     args = parse_parameters()
     pop_dir = Path(f"{args.population}_populations")
     pop_dir.mkdir(parents=True, exist_ok=True)
+    args.output.mkdir(parents=True, exist_ok=True)
 
-    # Load and impute data
-    logging.info("Loading PLINK variants...")
-    plink_path = here("input/simulations", pop_dir.name, "_m/plink-files/simulated")
-    variants_df = load_variants(plink_path)
-
-    logging.info("Reading RFMix outputs...")
-    binary_path = args.rfmix_input / "binary_files"
-    loci_df, _, admix = read_rfmix(here(args.rfmix_input), binary_dir=here(binary_path))
+    # Load imputed data
     method_path = here(args.rfmix_input / args.method)
     method_path.mkdir(parents=True, exist_ok=True)
     zarr_path = method_path / "imputed_local_ancestry"
+    variant_path = method_path / "imputed_variant.parquet"
 
-    logging.info("Interpolating ancestry data...")
-    loci_df = standardize_variant_columns(loci_df)
-    variant_loci_df = impute_data(
-        loci_df, variants_df, admix, zarr_path, args.method
-    )
-
-    # Filter and align
-    variant_loci_df = align_variants(variants_df, variant_loci_df)
+    logging.info("Loading imputed variants and ancestry data...")
+    variant_loci_df = pd.read_parquet(variant_path)
     idx = variant_loci_df.index.values
     inferred_anc_path = here(zarr_path / "local-ancestry.zarr")
     inferred_anc = da.from_zarr(inferred_anc_path)[idx, :, :]
