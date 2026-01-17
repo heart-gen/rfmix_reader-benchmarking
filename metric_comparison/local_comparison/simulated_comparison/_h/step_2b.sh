3a4a8ffb86a893c2e42b2dec18d71f4a73540c8e
Kynon J Benjamin <kj.benjamin90@gmail.com>
2026-01-17 09:36:12 -0500

    change environment for ML

--- metric_comparison/local_comparison/simulated_comparison/_h/step_2b.sh
+++ metric_comparison/local_comparison/simulated_comparison/_h/step_2b.sh
@@ -27,7 +27,7 @@ module load anaconda3/2024.10-1
 module list
 
 log_message "**** Loading conda environment ****"
-conda activate /ocean/projects/bio250020p/shared/opt/env/ai_env
+conda activate /ocean/projects/bio250020p/shared/opt/env/ml_dev
 
 log_message "**** Run analysis ****"
 CHR=${SLURM_ARRAY_TASK_ID}
